e9f2d3c00744924c1e670d84e76cfb4b4d6012a0
saneki <s@neki.me>
2015-09-23 13:08:53 -0700

    Use NUnit framework

--- src/EazvirtMe/Tests/EazCallTests.cs
+++ src/EazvirtMe/Tests/EazCallTests.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 using System.Reflection;
-using EazvirtMe.Attributes;
+using NUnit.Framework;
 
 namespace EazvirtMe.Tests
 {
-	[EazTest]
+	[TestFixture]
 	public class EazCallTests
 	{
+		[Test]
 		[Obfuscation(Feature = "virtualization", Exclude = false)]
-		public Boolean Test1()
+		public void Test1()
 		{
 			// Call another virtualized method
 			this.Test1_Called();
 
 			// Call another virtualized method, static
+			// ---
+			// Note: This causes an interesting issue: Both Test1 and Test1_Called_Static
+			// end up with the same method name after obfuscation (\0002). This seems to be
+			// because Eazfuscator has two "name counters," one for instance methods and
+			// another for static methods, per type. Because of this, when devirtualizing
+			// this method, the following call to Test1_Called_Static is resolved as a
+			// call to Test1, resulting in infinite recursion. This could be fixed if dnlib's
+			// SigComparer checked IsStatic when comparing MethodSigs (not sure why it doesn't).
+			//
+			// HOWEVER: Despite the infinite recursion, this function actually runs (and
+			// according to NUnit, passes). Although peverify complains about the infinite
+			// recursion as a "Stack underflow", the CLR seems to detect that
+			// Test1_Called_Static should be called instead, probably due to nothing being on
+			// the stack at the time of calling it (instance methods require the "this" param).
+			// ---
 			EazCallTests.Test1_Called_Static();
 
 			// Call another virtualized method, from an instance of another type
 			// and with a param + return value
 			var instanceClass = new Test1_InstanceClass();
-			if (!instanceClass.Test1_Called_OtherClass(1234).Equals("1234"))
-				return false;
-
-			return true;
+			Assert.AreEqual(instanceClass.Test1_Called_OtherClass(1234), "1234");
 		}
 
 		[Obfuscation(Feature = "virtualization", Exclude = false)]
