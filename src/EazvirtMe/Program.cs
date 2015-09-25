e2f2e756b6d63bd03d14c63474e1cdac846808e4
saneki <s@neki.me>
2015-09-25 11:07:01 -0700

    Removed old testing setup

--- src/EazvirtMe/Program.cs
+++ src/EazvirtMe/Program.cs
@@ -1,7 +1,6 @@
-﻿using EazvirtMe.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 // Disable string encryption for now
@@ -13,80 +12,9 @@ namespace EazvirtMe
 	{
 		static void Main(String[] args)
 		{
-			var assembly = Assembly.GetExecutingAssembly();
-			foreach (var type in assembly.DefinedTypes)
-			{
-				EazTestAttribute attribute;
-				if ((attribute = (EazTestAttribute)type.GetCustomAttribute(typeof(EazTestAttribute))) != null)
-					RunTests(type, attribute);
-			}
-		}
-
-		static void RunTests(TypeInfo typeInfo, EazTestAttribute attribute)
-		{
-			if (!typeInfo.IsClass)
-				return;
-
-			var possibleMethods = typeInfo.DeclaredMethods.Where(
-				m => m.IsPublic && !m.IsSpecialName);
-				// m.GetParameters().Length == (m.IsStatic ? 0 : 1));
-			var staticMethods = possibleMethods.Where(m => m.IsStatic);
-			var instanceMethods = possibleMethods.Where(m => !m.IsStatic);
-
-			Int32 staticCount = staticMethods.Count(), instanceCount = instanceMethods.Count();
-			if (staticCount > 0 || instanceCount > 0)
-			{
-				Console.WriteLine("> Type: {0}", typeInfo.FullName);
-
-				if (staticCount > 0)
-				{
-					Console.WriteLine(">>> Running static tests ({0})", staticMethods.Count());
-					Int32 successes = RunStaticTests(typeInfo, staticMethods);
-					Console.WriteLine(">>> [{0}/{1}] successful", successes, staticCount);
-				}
-
-				if (instanceCount > 0)
-				{
-					Console.WriteLine(">>> Running instance tests ({0})", instanceMethods.Count());
-					Int32 successes = RunInstanceTests(typeInfo, instanceMethods);
-					Console.WriteLine(">>> [{0}/{1}] successful", successes, instanceCount);
-				}
-			}
-			else Console.WriteLine("> Type (empty): {0}", typeInfo.FullName);
-		}
-
-		static Int32 RunStaticTests(TypeInfo typeInfo, IEnumerable<MethodInfo> methods)
-		{
-			Int32 successes = 0;
-			foreach (var method in methods)
-				if (LogMethodSuccess(method, method.Invoke(null, null)))
-					successes++;
-			return successes;
-		}
-
-		static Int32 RunInstanceTests(TypeInfo typeInfo, IEnumerable<MethodInfo> methods)
-		{
-			Int32 successes = 0;
-			Object instance = Activator.CreateInstance(typeInfo);
-			foreach (var method in methods)
-				if (LogMethodSuccess(method, method.Invoke(instance, null)))
-					successes++;
-			return successes;
-		}
-
-		static Boolean LogMethodSuccess(MethodInfo methodInfo, Object returnValue)
-		{
-			if (methodInfo.ReturnType == typeof(Boolean))
-			{
-				Boolean success = (Boolean)returnValue;
-				Console.WriteLine(">>> [{0}] {1}", success ? "Passed" : "Failed", methodInfo.Name);
-				return success;
-			}
-			else
-			{
-				Console.WriteLine(">>> [Passed] {0}", methodInfo.Name);
-				return true;
-			}
+			String filepath = Process.GetCurrentProcess().MainModule.FileName,
+			       filename = Path.GetFileName(filepath);
+			Console.WriteLine("usage: nunit-console {0}", filename);
 		}
 	}
 }
